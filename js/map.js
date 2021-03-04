5933ba8d0091c84844bc7babfa189b98e2ae8c03
rafayebra <77300814+rafayebra@users.noreply.github.com>
2021-03-04 18:04:32 +0100

    0403

--- js/map.js
+++ js/map.js
@@ -1,228 +1,178 @@
 
-    var mapMain;
-    var tb;
-    var lyrUSA;
-    var lyrCities;
 
-   
+require(["esri/map",
+"esri/layers/ArcGISDynamicMapServiceLayer",
 
-    require(["esri/map",
-        "esri/layers/ArcGISDynamicMapServiceLayer",
-        "esri/dijit/Legend",
-        "esri/dijit/BasemapGallery",
-        "esri/dijit/Search",
-        "esri/dijit/OverviewMap",
-        "esri/dijit/Scalebar",
-        "esri/toolbars/draw",
-        "esri/graphic",
-        "esri/layers/FeatureLayer",
-        "esri/tasks/query",
+"esri/dijit/HomeButton",
+"esri/dijit/Legend",
+"esri/dijit/BasemapGallery",
+"esri/dijit/Search",
+"esri/dijit/OverviewMap",
+"esri/dijit/Scalebar",
+"esri/toolbars/draw",
+"esri/graphic",
+"esri/layers/FeatureLayer",
+"esri/tasks/query",
 
-        "esri/symbols/SimpleFillSymbol",
-        "esri/symbols/SimpleLineSymbol",
-        "esri/symbols/SimpleMarkerSymbol",
-        "esri/Color",
-        
+"esri/dijit/Popup",
+"esri/dijit/PopupTemplate",
+"esri/InfoTemplate",
 
-        "dojo/on",
-
-        "dijit/layout/TabContainer",
-        "dijit/layout/ContentPane",
-        "dijit/layout/BorderContainer",
-        "dojo/domReady!"],
-        
-        function(
-          Map, ArcGISDynamicMapServiceLayer, Legend, BasemapGallery, Search, OverviewMap, Scalebar, Draw, Graphic, FeatureLayer, Query,
-          SimpleFillSymbol, SimpleLineSymbol, SimpleMarkerSymbol, Color, 
-          on
-
-        ) {
+"esri/symbols/SimpleFillSymbol",
+"esri/symbols/SimpleLineSymbol",
+"esri/symbols/SimpleMarkerSymbol",
+"esri/Color",
 
 
-        on(dojo.byId("pintaYQuery"),"click",fPintaYQuery);
-        on(dojo.byId("progButtonNode"),"click",fQueryEstados);
+"dojo/on",
 
-        
-          
+"dojo/dom-construct",
 
-          
+"dijit/layout/TabContainer",
+"dijit/layout/ContentPane",
+"dijit/layout/BorderContainer",
+"dojo/domReady!"],
 
-            function fPintaYQuery() {
-                var tbDraw = new Draw(mapMain);
+function(
+  Map, ArcGISDynamicMapServiceLayer, 
+  HomeButton, Legend, BasemapGallery, Search, OverviewMap, Scalebar, Draw, Graphic, FeatureLayer, Query,
+  Popup, PopupTemplate, InfoTemplate,
+  SimpleFillSymbol, SimpleLineSymbol, SimpleMarkerSymbol, Color, 
+   on, domConstruct,
 
-                tbDraw.on("draw-end", displayPolygon);
-                tbDraw.activate(Draw.POLYGON);
-            };
-
-            function displayPolygon(evt) {
-              var geometryInput = evt.geometry;
-
-            
-
-                    var tbDrawSymbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, 
-                        new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT, new Color([255, 255, 0]), 2), new Color([255, 255, 0, 0.2]));
-
-                    
-
-                    mapMain.graphics.clear();
-
-                  
-
-                    var graphicPolygon = new Graphic(geometryInput, tbDrawSymbol);
-                    mapMain.graphics.add(graphicPolygon);
-
-                    selectCities(geometryInput);
-                    };
-
-                    function selectCities(geometryInput) {
-
-                      
-                      var symbolSelected = new SimpleMarkerSymbol({
-                          "type": "esriSMS",
-                          "style": "esriSMSCircle",
-                          "color": [255, 115, 0, 128],
-                          "size": 6,
-                          "outline": {
-                              "color": [255, 0, 0, 214],
-                              "width": 1
-                          }
-                      });
-      
-      
-                      
-                      lyrCities.setSelectionSymbol(symbolSelected);
-      
-      
-                     
-      
-                      var queryCities = new Query();
-                      queryCities.geometry = geometryInput;  
-      
-      
-      
-                     
-      
-                      lyrCities.selectFeatures(queryCities, FeatureLayer.SELECTION_NEW)
-                      
-                  };
+) {
 
 
-              
-        
-         
-        function fQueryEstados(){
-         alert("Evento del botón Seleccionar ciudades");
-        }
+  
 
-        
-        
+  var popupOptions = {
+    fillSymbol: new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+                new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
+                new Color([255, 0, 0]), 2), new Color([255, 255, 0, 0.25])),
+    marginLeft: "20",
+    marginTop: "20"
+  };
+  //create a popup to replace the map's info window
+  var popup = new Popup(popupOptions, domConstruct.create("div"));
 
-        mapMain = new Map("map", {
-          basemap: "topo",         
-        });
 
-      
-        mapMain.on("load",function(evt){
-          mapMain.resize();
-          mapMain.reposition();
+  
+  
+  var mapMain = new Map("map", {
+      basemap: "topo",
+      infoWindow: popup         
+    });
 
-        
-          /*
-        Crear un widget Basemap Gallery. 
-        Es preciso la creación del nodo "basemapGallery" dentro de "map" en el código HTML.
-        */
-        var basemapGallery = new BasemapGallery({
-            showArcGISBasemaps: true,
-            map: mapMain,
-        }, "basemapGallery");
 
-        basemapGallery.startup()
+    var popupTemplate = new PopupTemplate({
 
-          /*
-        Crear un widget Search. 
-        Es preciso la creación del nodo "search" dentro de "map" en el código HTML.
-        */
+      title: "Estados: {STATE_NAME}",
+            fieldInfos: [{
+              fieldName: "st_area(shape)",
+              label: "Área:",
+              visible: true,
+              format: { places: 2 }
+            }, {
+              fieldName: "POP2000",
+              label: "Población:",
+              visible:true
+            }, {
+              fieldName: "POP00_sqmi",
+              label: "Población por milla cuadrada:",
+              visible: true                     
+            }]
+    });
 
-        var search = new Search({
-            map: mapMain,
-            autoComplete: true
-        }, "search");
+    mapMain.on("zoom",function(evt){
+      popup.hide()
+    });
 
-        search.startup();
+    /*
+    Añadir el USA map service al mapa
+    */
 
-          /*
-        Crear un widget OverviewMap. 
-        */
+   var lyrUSA = new ArcGISDynamicMapServiceLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/", {
+    opacity: 0,
+  });          
 
-        var overviewMapDijit = new OverviewMap({
-          map: mapMain,
-          attachTo:"bottom-right",
-          visible: true,
-          color:" #224a54",
-          opacity: 0.250
-           
-          
-         });
+    /*
+  Definir el feature layer del map service de forma independiente.
+  */
+  var lyrCities = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/0", {
+    opacity: 0.5,
+  }); 
 
-        overviewMapDijit.startup(); 
+  var lyrHighways = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/1", {
+    opacity: 0.5,
+  });
 
-           /*
-        Crear un widget de escala. 
-        */
+  var lyrStates = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/2", {
+    opacity: 0.5,
+    mode: FeatureLayer.MODE_SNAPSHOT,
+    infoTemplate: popupTemplate,
+    outFields: ["*"],
+  });
 
-        var dijitScalebar = new Scalebar({
-          map: mapMain,
-          scalebarUnit: "dual",
-          attachTo: "bottom-left",
-        });
+  var lyrCountries = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/3", {
+    opacity: 0.5,
+  });
 
-      
+
+    mapMain.on("load",function(evt){
+      mapMain.resize();
+      mapMain.reposition();
+
+    
+      mapMain.addLayers([lyrCountries, lyrStates, lyrHighways, lyrCities]);
 
         
-        
-        
-        
-          /*
-        Añadir el USA map service al mapa
-        */
+      /*
+    Crear un widget Basemap Gallery. 
+    Es preciso la creación del nodo "basemapGallery" dentro de "map" en el código HTML.
+    */
+    
+    var basemapGallery = new BasemapGallery({
+        showArcGISBasemaps: true,
+        map: mapMain,
+    }, "basemapGallery");
 
-        lyrUSA = new ArcGISDynamicMapServiceLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/", {
-          opacity: 0.5,
-        });          
+    basemapGallery.startup()
 
-        // mapMain.addLayers([lyrUSA]);
+      /*
+    Crear un widget Search. 
+    Es preciso la creación del nodo "search" dentro de "map" en el código HTML.
+    */
 
-          /*
-        Definir el feature layer de ciudades.
-        */
-        lyrCities = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/0", {
-          opacity: 0.5,
-        }
-        
-        );
+    var search = new Search({
+        map: mapMain,
+        autoComplete: true
+    }, "search");
 
-        mapMain.addLayers([lyrUSA, lyrCities]);
+    search.startup();
 
+      /*
+    Crear un widget OverviewMap. 
+    */
 
-         /*
-        Una vez que se hayan agregado todas las capas al mapa:
-        */
+    var overviewMapDijit = new OverviewMap({
+      map: mapMain,
+      attachTo:"bottom-right",
+      visible: true,
+      color:" #224a54",
+      opacity: 0.250      
+     });
 
-        mapMain.on("layers-add-result", function(){         
-          
-          // Centrar el mapa en la extensión del USA map service.
-          var extent = lyrUSA.fullExtent; 
-          mapMain.setExtent(extent);
+    overviewMapDijit.startup(); 
 
-          // Agregar una leyenda.
-          var dijitLegend = new Legend ({
-            map: mapMain,
-            arrangement: Legend.ALIGN_RIGHT,
-            },"legendDiv");
+       /*
+    Crear un widget de escala. 
+    */
 
-          dijitLegend.startup();
-          });
-
+    var dijitScalebar = new Scalebar({
+      map: mapMain,
+      scalebarUnit: "dual",
+      attachTo: "bottom-left",
+    }); 
 
   
 
@@ -231,9 +181,202 @@
 
 
 
+     //Create searchState widget
+     var searchStates = new Search({
+      map: mapMain,
+      
+      /*passing in empty source array to clear defaults such as "All" and the ArcGIS Online World Geocoding service
+      pasando una matriz de origen vacía para borrar valores predeterminados como "All" y el ArcGIS Online World Geocoding service
+      */
+      sources: [],
+     // zoomScale: 5000000
+      enableInfoWindow: true,
+      showInfoWindowOnSelect: true,
+   }, "searchStates");
 
-        });
+   /*listen for the load event and set the source properties. 
+   Escuche el evento de carga y establezca las propiedades de la fuente.
+   */
+   searchStates.on("load", function () {
+
+      var sources = searchStates.sources;
+      sources.push({
+         featureLayer: lyrStates,
+         placeholder: "Ohio",
+         enableLabel: true,
+         searchFields: ["state_name"],
+         displayField: "state_name",
+         exactMatch: false,
+         outFields: ["*"],
+
+         /* Create an InfoTemplate and include three fields
+         Cree una plantilla de información e incluya tres campos
+         */
+        // infoTemplate: new InfoTemplate("state_name")
+       //  infoWindow: popup,
+          infoTemplate: popupTemplate,
+
 
       });
 
+      /*Set the sources above to the search widget
+      Establecer las fuentes de arriba en el widget de búsqueda
+      */
+      searchStates.set("sources", sources);
+      
+      
+   });
+   searchStates.startup();
+   
+
+
+
+
+
+
+
+    
+    
+    
+    
+
+
+
+  
+
+
+     /*
+    Una vez que se hayan agregado todas las capas al mapa:
+    */
+
+    mapMain.on("layers-add-result", function(){         
+      
+      // Centrar el mapa en la extensión del USA map service.
+      var extent = lyrUSA.fullExtent; 
+      mapMain.setExtent(extent);
+
+      // Agregar una leyenda.
+      var dijitLegend = new Legend ({
+        map: mapMain,
+        arrangement: Legend.ALIGN_RIGHT,
+        },"legendDiv");
+
+      dijitLegend.startup();
+    
+      
+        /*
+      Crear un Home Botton. 
+      Es preciso la creación del nodo "basemapGallery" dentro de "map" en el código HTML.
+      */
+
+      var homeButton = new HomeButton({
+        theme: "HomeButton",
+        map: mapMain,
+        extent: extent,
+        visible: true
+      }, "homeBottom");
+      homeButton.startup();
+
+      });
+
+
+      on(dojo.byId("pintaYQuery"),"click",fPintaYQuery);
+      
+      // Se saca fuera la variable tbDraw para que también pueda leerla la función ffinPintaYQuery.
+      var tbDraw = new Draw(mapMain);
+      
+      function fPintaYQuery() {
+
+      mapMain.setInfoWindowOnClick(false);
+        
+        // var tbDraw = new Draw(mapMain);
+
+          tbDraw.on("draw-end", displayPolygon);
+          tbDraw.activate(Draw.POLYGON);
+      };
+
+      function displayPolygon(evt) {
+        var geometryInput = evt.geometry;
+
+      
+
+              var tbDrawSymbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, 
+                  new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT, new Color([255, 255, 0]), 2), new Color([255, 255, 0, 0.2]));
+
+              
+
+              mapMain.graphics.clear();
+
+            
+
+              var graphicPolygon = new Graphic(geometryInput, tbDrawSymbol);
+              mapMain.graphics.add(graphicPolygon);
+
+              selectCities(geometryInput);
+              };
+
+              function selectCities(geometryInput) {
+
+                
+                var symbolSelected = new SimpleMarkerSymbol({
+                    "type": "esriSMS",
+                    "style": "esriSMSCircle",
+                    "color": [255, 115, 0, 128],
+                    "size": 6,
+                    "outline": {
+                        "color": [255, 0, 0, 214],
+                        "width": 1
+                    }
+                });
+
+
+                
+                lyrCities.setSelectionSymbol(symbolSelected);
+
+
+                
+
+                var queryCities = new Query();
+                queryCities.geometry = geometryInput;  
+
+
+
+                
+
+                lyrCities.selectFeatures(queryCities, FeatureLayer.SELECTION_NEW)
+                
+            };
+  
+  
+      on(dojo.byId("finPintaYQuery"),"click",ffinPintaYQuery); 
+
+      
+      function ffinPintaYQuery(){      
+        lyrCities.clearSelection();
+        mapMain.graphics.clear();
+        tbDraw.deactivate();
+
+        mapMain.setInfoWindowOnClick(true);
+      };
+      
+      
+
+      on(dojo.byId("progButtonNode"),"click",fQueryEstados);
+       
+      function fQueryEstados(){
+       alert("Evento del botón Seleccionar ciudades");
+      }
+  
+      
+      
+
+
+
+
+
+
+
+    });
+
+  });
 
